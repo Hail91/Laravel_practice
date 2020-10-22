1145fb391559ea233faa5dd346d884509ab8bacc
Aaron <aarongillies88@gmail.com>
2020-10-22 11:51:04 -0400

    finished basic implementation of Laravel routes/controllers/models

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Course;
 
 class AppController extends Controller {
     public function index() {
-        return view('library');
+        return view('library', ['courses' => Course::all()]);
     }
 }
