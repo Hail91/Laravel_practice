8b5a467e972e89b05e2219652e4a44f0b88312f9
Aaron <aarongillies88@gmail.com>
2020-10-22 10:16:18 -0400

    added basic routing/controller examples

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,25 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+// Route to Welcome page (using AppController which will return welcome page)
+Route::get('/', 'App\Http\Controllers\AppController@index');
+// Route to tracks page
+Route::get('/tracks', function () {
     return view('welcome');
 });
+// Route to community page
+Route::get('/community', function () {
+    return view('welcome');
+});
+// Route to support page
+Route::get('/support', function () {
+    return view('welcome');
+});
+
+// Example routes to demonstrate closures in PHP/Laravel
+// Closures in PHP are just anonymous functions to perform some kind of code/execution
+// They are especially useful in routing, which we'll demonstrate below.
+// Route::get('/', function () {
+//     $number = 50;
+//     return $number * 10;
+// });
